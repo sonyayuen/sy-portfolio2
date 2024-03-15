00b6dca362f57ad5cc8ea057fc0030216d2019c1
Yuen, Sonya Luna <syuen@gatech.edu>
2024-03-14 20:10:43 -0400

    edits

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
 
 
 
-  <BrowserRouter  > 
+  <BrowserRouter basename={"/<sy-portfolio2/"} > 
     <Routes> 
       
       <Route path= "/" element={<Home  />} /> //sets home page
