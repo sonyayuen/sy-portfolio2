245097ebadc03040b54ecf5cb176d70762758915
Yuen, Sonya Luna <syuen@gatech.edu>
2024-03-29 00:42:00 -0400

    added http

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { qrcode } from 'vite-plugin-qrcode';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), qrcode()],
-  
+  resolve: {
+    alias: [{find: '@', replacement: path.resolve(__dirname, "src")}]
+  },
   base: "/sy-portfolio2/"
 })
