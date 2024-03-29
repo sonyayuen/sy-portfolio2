11eaa949e64aa67adcf311e25f7aa9d44209dfbd
Yuen, Sonya Luna <syuen@gatech.edu>
2024-03-28 22:16:39 -0400

    edit

--- src/scenes/sheCodes/index.tsx
+++ src/scenes/sheCodes/index.tsx
@@ -286,7 +286,7 @@ const SheCodes = () => {
                         <div className='w-full flex justify-center'>
                             <img src={sheCodesProject} alt="" className='object-cover rounded-3xl' />
                             
-                            
+                            <h1>tetsigndjgndkj</h1>
                             
                         </div>
 
