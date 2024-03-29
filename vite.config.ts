389527f48146ea67c3edfc7d550540b95a289e4f
Yuen, Sonya Luna <syuen@gatech.edu>
2024-03-29 00:36:32 -0400

    removed section in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import { qrcode } from 'vite-plugin-qrcode';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), qrcode()],
-  resolve: {
-    alias: [{find: '@', replacement: path.resolve(__dirname, "src")}]
-  },
-  base: "/sy-portfolio2"
+  
+  base: "/sy-portfolio2/"
 })
